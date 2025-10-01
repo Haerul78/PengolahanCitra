2ffb07333f69dc27d63e952c8de18c59cfd64e7c
Haerul78 <haerulrn20@gmail.com>
2025-10-02 00:50:49 +0700

    tambah layout dan fitur load grayscale dan save to txt

--- PengolahanCitra/Form1.Designer.cs
+++ PengolahanCitra/Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,41 +15,193 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.panelSidebar = new System.Windows.Forms.Panel();
+            this.btnSaveToTxt = new System.Windows.Forms.Button();
+            this.btnGrayScale = new System.Windows.Forms.Button();
+            this.btnBukaGambar = new System.Windows.Forms.Button();
+            this.panelToolbar = new System.Windows.Forms.Panel();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.pictureBoxMain = new System.Windows.Forms.PictureBox();
+            this.labelImageInfo = new System.Windows.Forms.Label();
+            this.panelSidebar.SuspendLayout();
+            this.panelToolbar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // panelSidebar
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.panelSidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.panelSidebar.Controls.Add(this.btnSaveToTxt);
+            this.panelSidebar.Controls.Add(this.btnGrayScale);
+            this.panelSidebar.Controls.Add(this.btnBukaGambar);
+            this.panelSidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSidebar.Location = new System.Drawing.Point(0, 60);
+            this.panelSidebar.Name = "panelSidebar";
+            this.panelSidebar.Size = new System.Drawing.Size(260, 640);
+            this.panelSidebar.TabIndex = 0;
+            // 
+            // btnSaveToTxt
+            // 
+            this.btnSaveToTxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(70)))));
+            this.btnSaveToTxt.FlatAppearance.BorderSize = 0;
+            this.btnSaveToTxt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveToTxt.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnSaveToTxt.ForeColor = System.Drawing.Color.White;
+            this.btnSaveToTxt.Location = new System.Drawing.Point(15, 128);
+            this.btnSaveToTxt.Margin = new System.Windows.Forms.Padding(3, 3, 3, 5);
+            this.btnSaveToTxt.Name = "btnSaveToTxt";
+            this.btnSaveToTxt.Size = new System.Drawing.Size(230, 45);
+            this.btnSaveToTxt.TabIndex = 2;
+            this.btnSaveToTxt.Text = "Simpan ke .Txt";
+            this.btnSaveToTxt.UseVisualStyleBackColor = false;
+            // 
+            // btnGrayScale
+            // 
+            this.btnGrayScale.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(70)))));
+            this.btnGrayScale.FlatAppearance.BorderSize = 0;
+            this.btnGrayScale.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGrayScale.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btnGrayScale.ForeColor = System.Drawing.Color.White;
+            this.btnGrayScale.Location = new System.Drawing.Point(15, 75);
+            this.btnGrayScale.Margin = new System.Windows.Forms.Padding(3, 3, 3, 5);
+            this.btnGrayScale.Name = "btnGrayScale";
+            this.btnGrayScale.Size = new System.Drawing.Size(230, 45);
+            this.btnGrayScale.TabIndex = 1;
+            this.btnGrayScale.Text = "GrayScale";
+            this.btnGrayScale.UseVisualStyleBackColor = false;
+            this.btnGrayScale.Click += new System.EventHandler(this.btnGrayscale_Click);
+            // 
+            // btnBukaGambar
+            // 
+            this.btnBukaGambar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnBukaGambar.FlatAppearance.BorderSize = 0;
+            this.btnBukaGambar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBukaGambar.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnBukaGambar.ForeColor = System.Drawing.Color.White;
+            this.btnBukaGambar.Location = new System.Drawing.Point(15, 20);
+            this.btnBukaGambar.Name = "btnBukaGambar";
+            this.btnBukaGambar.Size = new System.Drawing.Size(230, 45);
+            this.btnBukaGambar.TabIndex = 0;
+            this.btnBukaGambar.Text = "Buka Gambar";
+            this.btnBukaGambar.UseVisualStyleBackColor = false;
+            this.btnBukaGambar.Click += new System.EventHandler(this.btnBukaGambar_Click);
+            this.btnBukaGambar.MouseEnter += new System.EventHandler(this.Button_MouseEnter);
+            this.btnBukaGambar.MouseLeave += new System.EventHandler(this.Button_MouseLeave);
+            // 
+            // panelToolbar
+            // 
+            this.panelToolbar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panelToolbar.Controls.Add(this.btnSettings);
+            this.panelToolbar.Controls.Add(this.btnEdit);
+            this.panelToolbar.Controls.Add(this.btnHome);
+            this.panelToolbar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelToolbar.Location = new System.Drawing.Point(0, 0);
+            this.panelToolbar.Name = "panelToolbar";
+            this.panelToolbar.Size = new System.Drawing.Size(1200, 60);
+            this.panelToolbar.TabIndex = 1;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.btnSettings.ForeColor = System.Drawing.Color.White;
+            this.btnSettings.Location = new System.Drawing.Point(135, 10);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(50, 40);
+            this.btnSettings.TabIndex = 2;
+            this.btnSettings.Text = "⚙️";
+            this.btnSettings.UseVisualStyleBackColor = false;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.btnEdit.FlatAppearance.BorderSize = 0;
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEdit.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.btnEdit.ForeColor = System.Drawing.Color.White;
+            this.btnEdit.Location = new System.Drawing.Point(75, 10);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(50, 40);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "✏️";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            // 
+            // btnHome
+            // 
+            this.btnHome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.btnHome.ForeColor = System.Drawing.Color.White;
+            this.btnHome.Location = new System.Drawing.Point(15, 10);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(50, 40);
+            this.btnHome.TabIndex = 0;
+            this.btnHome.Text = "🏠";
+            this.btnHome.UseVisualStyleBackColor = false;
+            // 
+            // pictureBoxMain
+            // 
+            this.pictureBoxMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.pictureBoxMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxMain.Location = new System.Drawing.Point(260, 60);
+            this.pictureBoxMain.Name = "pictureBoxMain";
+            this.pictureBoxMain.Size = new System.Drawing.Size(940, 610);
+            this.pictureBoxMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxMain.TabIndex = 2;
+            this.pictureBoxMain.TabStop = false;
+            // 
+            // labelImageInfo
+            // 
+            this.labelImageInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.labelImageInfo.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.labelImageInfo.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.labelImageInfo.ForeColor = System.Drawing.Color.White;
+            this.labelImageInfo.Location = new System.Drawing.Point(260, 670);
+            this.labelImageInfo.Name = "labelImageInfo";
+            this.labelImageInfo.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.labelImageInfo.Size = new System.Drawing.Size(940, 30);
+            this.labelImageInfo.TabIndex = 3;
+            this.labelImageInfo.Text = "Ready";
+            this.labelImageInfo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.labelImageInfo.Click += new System.EventHandler(this.labelImageInfo_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(1200, 700);
+            this.Controls.Add(this.pictureBoxMain);
+            this.Controls.Add(this.labelImageInfo);
+            this.Controls.Add(this.panelSidebar);
+            this.Controls.Add(this.panelToolbar);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "Pengolahan Citra";
+            this.panelSidebar.ResumeLayout(false);
+            this.panelToolbar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelSidebar;
+        private System.Windows.Forms.Panel panelToolbar;
+        private System.Windows.Forms.PictureBox pictureBoxMain;
+        private System.Windows.Forms.Button btnBukaGambar;
+        private System.Windows.Forms.Button btnGrayScale;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Label labelImageInfo;
+        private System.Windows.Forms.Button btnSaveToTxt;
     }
 }
-
--- PengolahanCitra/Form1.cs
+++ PengolahanCitra/Form1.cs
@@ -1,23 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PengolahanCitra
 {
     public partial class Form1 : Form
     {
+        private Bitmap originalImage;
+        private Bitmap currentImage;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        // Event Handler untuk Buka Gambar
+        private void btnBukaGambar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+            openFileDialog.Title = "Pilih Gambar";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                originalImage = new Bitmap(openFileDialog.FileName);
+                currentImage = new Bitmap(originalImage);
+                pictureBoxMain.Image = currentImage;
+
+                labelImageInfo.Text = $"Gambar: {openFileDialog.SafeFileName} | Ukuran: {currentImage.Width}x{currentImage.Height}px";
+            }
+        }
+
+
+        // Event Handler untuk Grayscale
+        private void btnGrayscale_Click(object sender, EventArgs e)
+        {
+            if (currentImage == null)
+            {
+                MessageBox.Show("Silakan buka gambar terlebih dahulu!", "Peringatan");
+                return;
+            }
+
+            Bitmap grayImage = new Bitmap(currentImage.Width, currentImage.Height);
+
+            for (int x = 0; x < currentImage.Width; x++)
+            {
+                for (int y = 0; y < currentImage.Height; y++)
+                {
+                    Color originalColor = currentImage.GetPixel(x, y);
+                    int grayScale = (int)((originalColor.R * 0.3) + (originalColor.G * 0.59) + (originalColor.B * 0.11));
+                    Color grayColor = Color.FromArgb(grayScale, grayScale, grayScale);
+                    grayImage.SetPixel(x, y, grayColor);
+                }
+            }
+
+            currentImage = grayImage;
+            pictureBoxMain.Image = currentImage;
+            labelImageInfo.Text = "Grayscale diterapkan";
+        }
+
+        // Hover effect untuk button
+        private void Button_MouseEnter(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.BackColor = Color.FromArgb(0, 140, 220);
+        }
+
+        private void Button_MouseLeave(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (btn == btnBukaGambar)
+                btn.BackColor = Color.FromArgb(0, 122, 204);
+            else
+                btn.BackColor = Color.FromArgb(63, 63, 70);
+        }
+
+        private void labelImageInfo_Click(object sender, EventArgs e)
         {
 
         }
